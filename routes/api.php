96e31d7dca275d4b37b9a6b3bd08068da7f1248c
José Vitor <Josevitornascimentolopes@gmail.com>
2025-10-29 08:23:33 -0400

    adicionado sistema de notificação por nome de imagem. Ex: 'Lote 1: minha_foto_2_update_1.jpg'

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\Api\ImageController;
 use Illuminate\Support\Facades\Route;
 
-// Rota pública para listar todas as URLs de imagens
-Route::get('/images', [ImageController::class, 'index']);
-
-// NOVA ROTA: Lista imagens por categoria
+// ROTA EXISTENTE: Lista imagens por categoria com paginação
 // Exemplo de URL: /api/images/bom-dia?page=1
 Route::get('images/{category}', [ImageController::class, 'indexByCategory']);
+
+// NOVA ROTA DE NOTIFICAÇÃO: Retorna o lote de atualização COMPLETO (o Front-end gerencia o parcelamento diário)
+Route::get('notifications/latest-batch', [ImageController::class, 'getLatestNotificationBatch']);
